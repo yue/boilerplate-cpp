f467fd130088fba92e4b7d38f03f73bb79af3339
Cheng Zhao <zcbenz@gmail.com>
2018-02-16 17:58:39 +0900

    Put CMakeLists in root dir

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -8,10 +8,10 @@ mkdir('out')
 
 if (process.platform == 'win32') {
   const arch = targetCpu == 'x64' ? 'Win64' : 'Win32'
-  spawnSync(cmake, ['../src', '-G', `Visual Studio 14 ${arch}`])
+  spawnSync(cmake, ['..', '-G', `Visual Studio 14 ${arch}`])
 } else {
   mkdir('out/Release')
-  spawnSync(cmake, ['-D', `CMAKE_BUILD_TYPE=Release`, '../../src'], {cwd: 'out/Release'})
+  spawnSync(cmake, ['-D', `CMAKE_BUILD_TYPE=Release`, '../..'], {cwd: 'out/Release'})
   mkdir('out/Debug')
-  spawnSync(cmake, ['-D', `CMAKE_BUILD_TYPE=Debug`, '../../src'], {cwd: 'out/Debug'})
+  spawnSync(cmake, ['-D', `CMAKE_BUILD_TYPE=Debug`, '../..'], {cwd: 'out/Debug'})
 }
