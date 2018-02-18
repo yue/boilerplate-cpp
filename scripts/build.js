ac448a782f55472d865ab62a9e86d567ed8455e8
Cheng Zhao <zcbenz@gmail.com>
2018-02-18 11:29:52 +0900

    Fix building for x86 Windows

--- scripts/build.js
+++ scripts/build.js
@@ -7,9 +7,10 @@ const config = process.argv[2] === 'Release' ? 'Release' : 'Debug'
 
 if (process.platform == 'win32') {
   const msbuild = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe'
+  const platform = targetCpu == 'x64' ? 'x64' : 'Win32'
   process.exit(spawnSync(
       msbuild,
-      [`${name}.sln`, `/p:Configuration=${config}`, `/p:Platform=${targetCpu}`],
+      [`${name}.sln`, `/p:Configuration=${config}`, `/p:Platform=${platform}`],
       {cwd: 'out'}).status)
 } else if (process.platform == 'darwin') {
   process.exit(spawnSync(
