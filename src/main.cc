b37fb8c220fb974838677fb92dce8acb20344544
Cheng Zhao <zcbenz@gmail.com>
2018-02-23 20:34:16 +0900

    Implement toolbar on Debug mode

--- src/main.cc
+++ src/main.cc
@@ -65,17 +65,81 @@ int main(int argc, const char *argv[]) {
     nu::MessageLoop::Quit();
   });
 
+  // The content view.
+  nu::Container* content_view = new nu::Container;
+  window->SetContentView(content_view);
+
+#ifndef NDEBUG
+  // Browser toolbar.
+  nu::Container* toolbar = new nu::Container;
+  toolbar->SetStyle("flex-direction", "row", "padding", 5.f);
+  content_view->AddChildView(toolbar);
+  nu::Button* go_back = new nu::Button("<");
+  go_back->SetEnabled(false);
+  toolbar->AddChildView(go_back);
+  nu::Button* go_forward = new nu::Button(">");
+  go_forward->SetEnabled(false);
+  go_forward->SetStyle("margin-right", 5.f);
+  toolbar->AddChildView(go_forward);
+  nu::Entry* address_bar = new nu::Entry;
+  address_bar->SetStyle("flex", 1.f);
+  toolbar->AddChildView(address_bar);
+#endif
+
   // Create the webview.
+  nu::Browser::Options options;
+#ifndef NDEBUG
+  options.devtools = true;
+  options.context_menu = true;
+#endif
+  nu::Browser* browser(new nu::Browser(options));
+  browser->SetStyle("flex", 1.f);
+  content_view->AddChildView(browser);
+
+#ifndef NDEBUG
+  // Bind webview events to toolbar.
+  browser->on_update_title.Connect([](nu::Browser* self,
+                                      const std::string& title) {
+    self->GetWindow()->SetTitle(title);
+  });
+  browser->on_update_command.Connect([=](nu::Browser* self) {
+    go_back->SetEnabled(self->CanGoBack());
+    go_forward->SetEnabled(self->CanGoForward());
+    address_bar->SetText(self->GetURL());
+  });
+  browser->on_commit_navigation.Connect([=](nu::Browser* self,
+                                            const std::string& url) {
+    address_bar->SetText(url);
+  });
+  browser->on_finish_navigation.Connect([](nu::Browser* self,
+                                           const std::string& url) {
+    self->Focus();
+  });
+
+  // Bind toolbar events to browser.
+  address_bar->on_activate.Connect([=](nu::Entry* self) {
+    browser->LoadURL(self->GetText());
+  });
+  go_back->on_click.Connect([=](nu::Button* self) {
+    browser->GoBack();
+  });
+  go_forward->on_click.Connect([=](nu::Button* self) {
+    browser->GoForward();
+  });
+#endif
+
+  // Set webview bindings and custom protocol.
   nu::Browser::RegisterProtocol("muban", &CustomProtocolHandler);
-  scoped_refptr<nu::Browser> browser(new nu::Browser());
   browser->SetBindingName("muban");
   browser->AddBinding("showSysInfo", &ShowSysInfo);
-  window->SetContentView(browser.get());
 
   // Show window when page is loaded.
-  browser->on_finish_navigation.Connect([](nu::Browser* browser,
-                                           const std::string& url) {
-    browser->GetWindow()->Activate();
+  int id = -1;
+  id = browser->on_finish_navigation.Connect([=](nu::Browser* self,
+                                                 const std::string& url) {
+    self->GetWindow()->Activate();
+    // Only activate for the first time.
+    self->on_finish_navigation.Disconnect(id);
   });
   browser->LoadURL("muban://app/index.html");
 
