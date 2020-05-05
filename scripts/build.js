b2e3f5a6eb9a52144ffeaf7c6cce44d14cda5a58
Cheng Zhao <zcbenz@gmail.com>
2020-05-05 17:39:35 +0900

    Use Azure Pipelines

--- scripts/build.js
+++ scripts/build.js
@@ -2,16 +2,23 @@
 
 const {targetCpu, spawnSync} = require('./common')
 
+const path = require('path')
+
 const name = require('../package.json').name
 const config = process.argv[2] ? process.argv[2] : 'Debug'
 
 if (process.platform == 'win32') {
-  const msbuild = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe'
+  const vsPaths = [
+    'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin',
+    'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\MSBuild\\15.0\\Bin',
+    process.env.PATH
+  ]
+  const env = Object.assign(process.env, {PATH: vsPaths.join(path.delimiter)})
   const platform = targetCpu == 'x64' ? 'x64' : 'Win32'
   process.exit(spawnSync(
-      msbuild,
-      [`${name}.sln`, `/p:Configuration=${config}`, `/p:Platform=${platform}`],
-      {cwd: 'out'}).status)
+      'msbuild',
+      [name + '.sln', '/p:Configuration=' + config, '/p:Platform=' + platform],
+      {cwd: 'out', env}).status)
 } else if (process.platform == 'darwin') {
   process.exit(spawnSync(
       'xcodebuild',
